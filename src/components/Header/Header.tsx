dd2901234dbd129da6e7aede0ed7870c276321d0
Natalia <natalia-321s@outlook.com>
2022-12-22 16:47:49 -0300

    feat: estilos do componente Header
    add: svg usado na borda do header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,16 @@
 import { List } from 'phosphor-react';
 import Logo from '/assets/logo.svg';
 
+import './styles.css';
+
 export function Header() {
+  const corPrimaria = '#C3543E'
   return (
     <header>
       <img src={Logo} alt='Logo da padaria' />
-      <List size={32} />
-      {/* A minha ideia inicial para fazer a divisória seria como uma bg image */}
+      <button className='header-menu'>
+        <List size={32} color={corPrimaria}/>
+      </button>
     </header>
   );
 }
